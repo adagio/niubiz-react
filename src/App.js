b7da2c8ba33fafaa490e4920f5ffac9a1faa453d
Franco Cedillo <fcedillo@fintechlab.pe>
2020-12-15 07:27:15 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+        <h1 class="text-center">Pago con Visa</h1>
+        
+        <h3>Información del pago</h3>
+        <b>Importe a pagar: </b> $ 1 <br />
+        <b>Número de pedido: </b>  <br />
+        <b>Concepto: </b> Detalle de pago <br />
+        <b>Fecha: </b> 12/12/2020 <br />
+        
+        
+        <input type="checkbox" name="ckbTerms" id="ckbTerms" onclick="visaNetEc3()" /> <label for="ckbTerms">Acepto los <a href="#" target="_blank">Términos y condiciones</a></label>
+        <form id="frmVisaNet" action="http://localhost:8082/PagoWebPhp/finalizar.php?amount=1&purchaseNumber=1">
+            <script src="https://static-content-qas.vnforapps.com/v2/js/checkout.js?qa=true" 
+                data-sessiontoken="84182eb6d9a65acbdbccd26913462a054a8c4ee084a6ae3feb39d589cf016892"
+                data-channel="web"
+                data-merchantid="750000004"
+                data-merchantlogo="http://localhost:8082/PagoWebPhp/assets/img/logo.png"
+                data-purchasenumber="3412312300"
+                data-amount="1"
+                data-expirationminutes="5"
+                data-timeouturl="http://localhost:8082/PagoWebPhp/"
+            ></script>
+        </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Inicio from './Inicio';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Inicio name="Liam"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
